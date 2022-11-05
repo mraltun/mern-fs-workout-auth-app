68fcc66e26c854bbd1107a27db15610bc03805f7
Murat Altun <mraltun@gmail.com>
2022-11-05 15:17:43 +0300

    Added basic middleware

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,18 @@ require("dotenv").config();
 
 const app = express();
 
+// Middlewares
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// Routes;
 app.get("/", (req, res) => {
   res.json({ msg: "Welcome the the app" });
 });
 
-app.listen(process.env.PORT || 4000, () => {
-  console.log("Listening on port 4000");
+// Server start
+app.listen(process.env.PORT || 8000, () => {
+  console.log("Listening on port 3000");
 });
