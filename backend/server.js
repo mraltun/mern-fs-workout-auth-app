89d9f27f7e2beee317d4752b99fc83c092df0116
Murat Altun <mraltun@gmail.com>
2022-11-05 14:54:11 +0300

    Added main route and installed dotenv for the env file

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 
-app.listen(4000, () => {
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome the the app" });
+});
+
+app.listen(process.env.PORT || 4000, () => {
   console.log("Listening on port 4000");
 });
