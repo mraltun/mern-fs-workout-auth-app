420d5fd10ba17bf90beeb6332a2d3ad0a266f277
Murat Altun <mraltun@gmail.com>
2022-11-05 16:14:49 +0300

    Added MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 
 const app = express();
@@ -14,7 +15,15 @@ app.use((req, res, next) => {
 // Routes;
 app.get("/api/workouts", workoutRoutes);
 
-// Server start
-app.listen(process.env.PORT || 8000, () => {
-  console.log("Listening on port 3000");
-});
+// Connect to DB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // Server start
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Connected to DB & Listening on port 8000");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
