213e1ed2f7d5363e8d200982eba911030236be0c
Murat Altun <mraltun@gmail.com>
2022-11-06 13:42:54 +0300

    Added Navbar and index.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
+        <Navbar />
         <div className='pages'>
           <Routes>
             <Route path='/' element={<Home />} />
