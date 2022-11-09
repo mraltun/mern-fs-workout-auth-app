4bfc4d6ddc72e772af59b44a9f6b12b90d28382b
Murat Altun <mraltun@gmail.com>
2022-11-09 13:00:25 +0300

    Added static signup method .Installed bcrypt

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
@@ -13,5 +14,20 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+// Custom static signup method, we gonna use "this" so no ES6
+userSchema.statics.signup = async function (email, password) {
+  // We can't use "User" so use "this" instead.
+  const exists = await this.findOne({ email });
+  if (exists) {
+    throw Error("Email already in use");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
