ec7d7a855d9a6914ed1de740e3d5d41c2efc679f
Murat Altun <mraltun@gmail.com>
2022-11-09 11:58:18 +0300

    Added user route and userController

---
+++ backend/controllers/userController.js
@@ -0,0 +1,11 @@
+// Login user
+const loginUser = async (req, res) => {
+  res.json({ msg: "Login user" });
+};
+
+// Signup user
+const signupUser = async (req, res) => {
+  res.json({ msg: "Signup user" });
+};
+
+module.exports = { loginUser, signupUser };
