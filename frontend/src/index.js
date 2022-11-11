da2433a9753d1c2e4c6f35fce475220deca3eaa0
Murat Altun <mraltun@gmail.com>
2022-11-11 18:20:28 +0300

    Added AuthContext

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { WorkoutsContextProvider } from "./context/WorkoutContext";
+import { AuthContextProvider } from "./context/AuthContext";
 import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <WorkoutsContextProvider>
-      <App />
-    </WorkoutsContextProvider>
+    <AuthContextProvider>
+      <WorkoutsContextProvider>
+        <App />
+      </WorkoutsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
