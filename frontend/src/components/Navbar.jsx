0093195070d1aa672f299456e7fb3435633f9079
Murat Altun <mraltun@gmail.com>
2022-12-01 13:31:00 +0300

    Added conditional checks in Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import useLogout from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
+
   const handleClick = () => {
     logout();
   };
@@ -15,13 +18,18 @@ const Navbar = () => {
           <h1>Workouts App</h1>
         </Link>
         <nav>
-          <div>
-            <button onClick={handleClick}>Logout</button>
-          </div>
-          <div>
-            <Link to='login'>Login</Link>
-            <Link to='signup'>Signup</Link>
-          </div>
+          {user && (
+            <div>
+              <span>{user.email}</span>
+              <button onClick={handleClick}>Logout</button>
+            </div>
+          )}
+          {!user && (
+            <div>
+              <Link to='login'>Login</Link>
+              <Link to='signup'>Signup</Link>
+            </div>
+          )}
         </nav>
       </div>
     </header>
